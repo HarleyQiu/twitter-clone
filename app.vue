05c4075f2b88c49a22ada9a59c84498d561dc717
QiuLinhao <210554114@qq.com>
2024-02-09 01:50:04 +0800

    功能: 添加响应式布局及暗黑模式支持
    - 使用 Tailwind CSS 实现响应式网格布局，从小屏幕的单列布局到大屏幕的多列布局自动调整。
    - 引入暗黑模式切换功能，允许用户动态地在亮色和暗色主题之间切换。
    - 添加一个固定侧边栏组件（`<SidebarLeft/>`），在滚动时保持在视口顶部可见。此侧边栏在中屏幕及以下隐藏，体现移动优先的设计理念。
    - 利用 Tailwind 的间距、网格布局和背景颜色实用类来增强 UI 的响应性和美观。
    
    已在各种设备尺寸上测试布局的响应性，确保了流畅的用户体验。暗黑模式支持通过 Vue 的响应式系统实现，为用户提供了高效且友好的主题切换体验。

--- app.vue
+++ app.vue
@@ -1,19 +1,25 @@
 <template>
+  <!-- 外层容器，支持暗黑模式切换 -->
   <div :class="{'dark': darkMode}">
-    <!-- <NuxtWelcome /> -->
+
+    <!-- 内层容器，确保内容至少填满整个视口的高度 -->
     <div class="min-h-full">
 
+      <!-- 网格布局容器，定义为12列；自动边距居中；小屏幕以上的边距；大屏幕以上的最大宽度和边距设置；大屏幕以上的网格间距 -->
       <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
-        <!-- Left 侧边栏 -->
+
+        <!-- 左侧边栏，隐藏在中屏幕以下，背景色为红色，小屏幕占用1列，超大屏幕占用2列 -->
         <div class="hidden bg-red-500 md:block xs:col-span-1 xl:col-span-2">
+
+          <!-- 使侧边栏在滚动时保持在顶部 -->
           <div class="sticky top-0">
             <SidebarLeft/>
           </div>
         </div>
 
-        <!-- Main context -->
+        <!-- 主内容区域 -->
 
-        <!-- Right 侧边栏 -->
+        <!-- 右侧边栏 -->
 
       </div>
 
